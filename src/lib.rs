cf8ab7af94956abbd489bb45e53c514857b944ad
Yamakaky <yamakaky@yamaworld.fr>
2016-04-11 20:04:21 +0200

    Convert from binary to lib.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate enum_primitive;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate nom;
+extern crate num;
+
+pub mod cpu;
+pub mod iterators;
+pub mod parser;
+pub mod types;
