fab6782ae89d856b3871278afa8944a6d7938892
Yamakaky <yamakaky@yamaworld.fr>
2016-05-14 16:46:08 +0200

    Add expression solver with labels.

--- src/assembler/types.rs
+++ src/assembler/types.rs
@@ -1,4 +1,5 @@
-use types::{BasicOp, SpecialOp, Register};
+use std::collections::HashMap;
+use types::{BasicOp, SpecialOp, Register, Value, Instruction};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Directive {
@@ -11,13 +12,31 @@ pub enum ParsedItem {
     LabelDecl(String),
     LocalLabelDecl(String),
     ParsedInstruction(ParsedInstruction),
-    Comment(String)
+    Comment(String),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum ParsedInstruction {
     BasicOp(BasicOp, ParsedValue, ParsedValue),
-    SpecialOp(SpecialOp, ParsedValue)
+    SpecialOp(SpecialOp, ParsedValue),
+}
+
+impl ParsedInstruction {
+    fn solve(&self,
+             globals: &HashMap<String, u16>,
+             locals: &HashMap<String, u16>)
+             -> Result<Instruction, String> {
+        match *self {
+            ParsedInstruction::BasicOp(op, ref b, ref a) => {
+                Ok(Instruction::BasicOp(op,
+                                        try!(b.solve(globals, locals)),
+                                        try!(a.solve(globals, locals))))
+            }
+            ParsedInstruction::SpecialOp(op, ref a) => {
+                Ok(Instruction::SpecialOp(op, try!(a.solve(globals, locals))))
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -32,7 +51,30 @@ pub enum ParsedValue {
     PC,
     EX,
     AtAddr(Expression),
-    Litteral(Expression)
+    Litteral(Expression),
+}
+
+impl ParsedValue {
+    fn solve(&self,
+             globals: &HashMap<String, u16>,
+             locals: &HashMap<String, u16>)
+             -> Result<Value, String> {
+        match *self {
+            ParsedValue::Reg(r) => Ok(Value::Reg(r)),
+            ParsedValue::AtReg(r) => Ok(Value::AtReg(r)),
+            ParsedValue::AtRegPlus(r, ref e) => {
+                Ok(Value::AtRegPlus(r, try!(e.solve(globals, locals))))
+            }
+            ParsedValue::Push => Ok(Value::Push),
+            ParsedValue::Peek => Ok(Value::Peek),
+            ParsedValue::Pick(ref e) => Ok(Value::Pick(try!(e.solve(globals, locals)))),
+            ParsedValue::SP => Ok(Value::SP),
+            ParsedValue::PC => Ok(Value::PC),
+            ParsedValue::EX => Ok(Value::EX),
+            ParsedValue::AtAddr(ref e) => Ok(Value::AtAddr(try!(e.solve(globals, locals)))),
+            ParsedValue::Litteral(ref e) => Ok(Value::Litteral(try!(e.solve(globals, locals)))),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -46,20 +88,64 @@ pub enum Expression {
     Div(Box<Expression>, Box<Expression>),
     Shr(Box<Expression>, Box<Expression>),
     Shl(Box<Expression>, Box<Expression>),
-    Mod(Box<Expression>, Box<Expression>)
+    Mod(Box<Expression>, Box<Expression>),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+impl Expression {
+    fn solve(&self,
+             globals: &HashMap<String, u16>,
+             locals: &HashMap<String, u16>)
+             -> Result<u16, String> {
+        match *self {
+            Expression::Label(ref s) => {
+                match globals.get(s) {
+                    Some(addr) => Ok(*addr),
+                    None => Err(s.clone()),
+                }
+            }
+            Expression::LocalLabel(ref s) => {
+                match locals.get(s) {
+                    Some(addr) => Ok(*addr),
+                    None => Err(s.clone()),
+                }
+            }
+            Expression::Num(n) => Ok(n.into()),
+            Expression::Add(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)).wrapping_add(try!(r.solve(globals, locals))))
+            }
+            Expression::Sub(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)).wrapping_sub(try!(r.solve(globals, locals))))
+            }
+            Expression::Mul(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)).wrapping_mul(try!(r.solve(globals, locals))))
+            }
+            Expression::Div(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)).wrapping_div(try!(r.solve(globals, locals))))
+            }
+            Expression::Shr(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)) >> try!(r.solve(globals, locals)))
+            }
+            Expression::Shl(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)) << try!(r.solve(globals, locals)))
+            }
+            Expression::Mod(ref l, ref r) => {
+                Ok(try!(l.solve(globals, locals)) % try!(r.solve(globals, locals)))
+            }
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Num {
     U(u16),
-    I(i16)
+    I(i16),
 }
 
 impl From<Num> for u16 {
     fn from(n: Num) -> u16 {
         match n {
             Num::U(u) => u,
-            Num::I(i) => i as u16
+            Num::I(i) => i as u16,
         }
     }
 }
@@ -69,4 +155,3 @@ impl From<Num> for Expression {
         Expression::Num(n)
     }
 }
-
