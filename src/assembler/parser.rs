96373ef05fc859980398118c3e5193a1e9e8883d
Yamakaky <yamakaky@yamaworld.fr>
2016-06-30 13:47:27 +0200

    Support .word

--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -281,7 +281,7 @@ named!(string<String>,
 );
 
 named!(dir_dat<Directive>,
-    chain!(alt_complete!(tag!("dat") | tag!("byte")) ~
+    chain!(alt_complete!(tag!("dat") | tag!("byte")| tag!("word")) ~
            space ~
            ns: separated_list!(space,
                                alt_complete!(map!(number, From::from) |
