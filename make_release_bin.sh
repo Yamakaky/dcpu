07d8e02ea401eeefd31cbc407cd7f45e39a6d214
Yamakaky <yamakaky@yamaworld.fr>
2016-09-26 23:41:19 -0400

    Add missing binary

--- make_release_bin.sh
+++ make_release_bin.sh
@@ -6,4 +6,4 @@ cargo build --target=x86_64-unknown-linux-musl --release
 cargo build --target=x86_64-pc-windows-gnu --release
 
 mkdir -p target/bins
-cp target/*/release/{emulator,emulator.exe,assembler,assembler.exe,disassembler,disassembler.exe} target/bins/
+cp target/*/release/{emulator,emulator.exe,assembler,assembler.exe,disassembler,disassembler.exe,sprite,sprite.exe} target/bins/
