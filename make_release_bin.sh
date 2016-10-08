905e578c54e18d13cc60c6df2e3d9855c26a40cd
Yamakaky <yamakaky@yamaworld.fr>
2016-10-08 19:57:24 -0400

    Better release script

--- make_release_bin.sh
+++ make_release_bin.sh
@@ -5,5 +5,12 @@ IFS=$'\n\t'
 cargo build --target=x86_64-unknown-linux-musl --release
 cargo build --target=x86_64-pc-windows-gnu --release
 
+(
+    cd image
+    cargo build --target=x86_64-unknown-linux-musl --release
+    cargo build --target=x86_64-pc-windows-gnu --release
+)
+
 mkdir -p target/bins
-cp target/*/release/{emulator,emulator.exe,assembler,assembler.exe,disassembler,disassembler.exe,sprite,sprite.exe} target/bins/
+cp target/*/release/{emulator,emulator.exe,assembler,assembler.exe,disassembler,disassembler.exe} target/bins/
+cp image/target/*/release/{sprite,sprite.exe} target/bins
